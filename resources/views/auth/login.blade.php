331a1350a625511886fb05eb365aad9877b85ef1
Leonardo Ralph <theleoralph@gmail.com>
2024-11-07 14:48:09 -0300

    Login view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,43 @@
+<x-layouts.guest>
+    <div class="container">
+
+        <div class="row justify-content-center">
+
+            <div class="col-10 col-md-3">
+
+                <div class="row">
+
+                    <div class="col-12 text-center">
+                        <h2>Plenty</h2>
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-12">Usuário</div>
+
+                    <div class="col-12">Senha</div>
+
+                    <div class="col-12">Botão Lembrar</div>
+
+                    <div class="col-12 mt-4">Botão Login</div>
+                </div>
+
+                <hr>
+
+                <div class="row">
+
+                    <div class="col-12">
+
+                        Botão Criar Conta
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</x-layouts.guest>
