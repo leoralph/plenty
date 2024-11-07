e7efde7a46495a13491e07057e8f4cf26442617e
Fabricio-GuiFJ <fabriciojunior2@gmail.com>
2024-11-07 18:06:42 -0300

    Estilização e criação do form para tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,38 +1,50 @@
 <x-layouts.guest>
     <div class="container">
 
-        <div class="row justify-content-center">
+        <div class="space"></div>
+
+        <div class="row justify-content-center mt-4">
 
             <div class="col-10 col-md-3">
 
-                <div class="row">
+                <div class="row ">
 
-                    <div class="col-12 text-center">
+                    <div class="col-12  text-center">
                         <h2>Plenty</h2>
                     </div>
 
                 </div>
 
-                <div class="row">
+                <div class="row ">
 
-                    <div class="col-12">Usuário</div>
+                    
+                    <form >
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <label class="form-label" for="e-mail">{{__('E-mail')}} :</label>
+                            <input type="email" id="e-mail" class="form-control"/>
+            
+                        </div>
 
-                    <div class="col-12">Senha</div>
+                        <div data-mdb-input-init class="form-outline mb-4">
+                            <label class="form-label" for="password">{{__('Password')}} :</label>
+                            <input type="password" id="password" class="form-control" />
+                        </div>
 
-                    <div class="col-12">Botão Lembrar</div>
+                        <div class="col-12">
+                            <input type="checkbox" id="remember" checked/>
+                            <label for="remember">{{__("Remember-me")}}</label>
+                        </div>
 
-                    <div class="col-12 mt-4">Botão Login</div>
-                </div>
-
-                <hr>
-
-                <div class="row">
-
-                    <div class="col-12">
-
-                        Botão Criar Conta
-                    </div>
-
+                        <div class="row">
+                            <div class="col-6 mt-4 d-grid">
+                                <Button class="btn btn-outline-secondary" type="button" id="login" >{{__("Login")}}</Button>
+                                
+                            </div>
+                            <div class="col-6 mt-4 d-grid">
+                                <Button class="btn btn-outline-primary" type="button" id="sign-in">{{__("Sign-in")}}</Button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
 
             </div>
